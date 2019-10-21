79d1f25b6b8eb4a347ee301034ac93253ec64903
Thiery Louison <thiery.louison@gmail.com>
2019-10-21 13:30:07 +0400

    Add which aws profile to be used

---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "region" {
+  default = "us-west-1"
+}
+variable "AmiLinux" {
+  type = "map"
+  default = {
+    eu-west-1 = "ami-9398d3e0" # Ireland
+  }
+  description = "I add only 1 region, Ireland, to show the map feature but you can add all the regions that you need"
+}
+/*
+variable "aws_access_key" {
+  default = ""
+  description = "the user aws access key"
+}
+
+variable "aws_secret_key" {
+  default = ""
+  description = "the user aws secret key"
+}
+*/
+
+variable "aws_profie" {
+  default = ""
+  description = "the aws profile to be used"
+}
+
+variable "credentialsfile" {
+  default = "/Users/thieryl/.aws/credentials" #replace your home directory
+  description = "where your access and secret_key are stored, you create the file when you run the aws config"
+}
+
+variable "vpc-fullcidr" {
+    default = "172.28.0.0/16"
+  description = "the vpc cdir"
+}
+variable "Subnet-Public-AzA-CIDR" {
+  default = "172.28.0.0/24"
+  description = "the cidr of the subnet"
+}
+variable "Subnet-Private-AzA-CIDR" {
+  default = "172.28.3.0/24"
+  description = "the cidr of the subnet"
+}
+variable "key_name" {
+  default = "demotmp"
+  description = "the ssh key to use in the EC2 machines"
+}
+variable "DnsZoneName" {
+  default = "tricky-bit.internal"
+  description = "the internal dns name"
+}
